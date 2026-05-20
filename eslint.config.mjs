c04ae3d3d2455a582d83ea961929d0cde7ad5852
Nino Rodriguez <oninross@gmail.com>
2026-05-20 21:42:39 +1000

    feat: add ESLint and Prettier configuration files for code quality enforcement

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,31 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import prettierPlugin from "eslint-plugin-prettier";
 
-import { FlatCompat } from "@eslint/eslintrc";
-
-import storybook from "eslint-plugin-storybook";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({ baseDirectory: __dirname });
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+/** @type {import("eslint").Linter.FlatConfig[]} */
+export default [
   {
-    ignores: ["**/node_modules/**"],
+    ignores: ["node_modules/**", "dist/**", "storybook-static/**", "coverage/**"],
+  },
+  {
+    files: ["src/**/*.{ts,tsx}", "stories/**/*.{ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: "./tsconfig.app.json",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      "prettier/prettier": "error",
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    },
   },
 ];
-
-export default eslintConfig;
